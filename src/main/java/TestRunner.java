84a7fd58dcf803c41773603c477424370164f5ba
mootezbenabdallah96@gmail.com <mootez2106>
2023-11-12 13:56:38 +0100

    testng file

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -2,7 +2,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = {"src/main/java/Test.feature"},
+        features = {"src/main/java/xray-feature"},
         glue = "steps",
         plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json"})
 public class TestRunner extends AbstractTestNGCucumberTests {
